4a0f9bb19768eb554a94b79d2093016e3721ea28
Maksym Lvov <leva880@ukr.net>
2021-11-17 14:27:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const axios = require('axios');
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  container: document.querySelector('.gallery'),
+  loadMoreButton: document.querySelector('.load-more'),
+};
+
+refs.form.addEventListener('submit', onFormSumbit);
+
+function onFormSumbit(event) {
+    event.preventDefault();
+    
+    const inputValue = event.currentTarget.searchQuery.value;
+
+  console.log(inputValue);
+}
+
+// console.log(refs.loadMoreButton);
